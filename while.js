7e15b8066c4c2c60f7ce98a3bd63980a90923723
Kayathri2218 <kaya3kayu2000@gmail.com>
2023-02-01 14:19:06 -0500

    Adding loop.

---
+++ while.js
@@ -0,0 +1,26 @@
+document.write('<br>');
+//while loop
+
+document.write("while loop");
+document.write('<br>');
+var m = 1;
+while (m<=10) {
+    console.log(m);
+	document.write(m);
+	document.write('<br>');
+    m++;
+}
+document.write('<br>');
+//do while loop
+
+document.write("do...while loop");
+document.write('<br>');
+var M = 1;
+do{
+	console.log(M);
+	document.write(M);
+	document.write('<br>');
+    M++;
+} while (M<10);
+
+
